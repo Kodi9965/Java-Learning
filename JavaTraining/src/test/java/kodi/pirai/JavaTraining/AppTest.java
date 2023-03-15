5047593cc2a29c47f789902ff4b2f9602dcf02f9
User-L35 <User-L35@PI-IN-L35>
2023-03-15 18:02:08 +0530

    Sample1

---
+++ JavaTraining/src/test/java/kodi/pirai/JavaTraining/AppTest.java
@@ -0,0 +1,13 @@
+package kodi.pirai.JavaTraining;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
