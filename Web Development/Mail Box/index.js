50a574c31b29a106a3bf44296c425f051be876a8
Dhanushkodi <102147987+Kodi9965@users.noreply.github.com>
2023-04-28 09:57:29 +0530

    Add files via upload

---
+++ Web Development/Mail Box/index.js
@@ -0,0 +1,96 @@
+// let ham = document.getElementById('bar');
+// let menu = document.getElementById('Menu');
+// ham.addEventListener('click',()=>{
+//      menu.innerHTML=`
+//      <div class="menu" id="Menu">
+//           <div class="message mg">
+//             <div class="msg"><p>Message</p></div>
+//           </div>
+//           <div class="sideBar">
+//             <div class="ic">
+//               <div class="l">
+//                 <i class="fa-solid fa-inbox"></i><span>Inbox</span>
+//               </div>
+//               <div class="rating r1">12</div>
+//             </div>
+//             <div class="ic">
+//               <div class="l">
+//                 <i class="fa-regular fa-paper-plane"></i><span>Sent</span>
+//               </div>
+//               <!-- <div class="rating">12</div> -->
+//             </div>
+//             <div class="ic">
+//               <div class="l">
+//                 <i class="fa-regular fa-file"></i><span>Drafts</span>
+//               </div>
+//               <div class="rating r2">20</div>
+//             </div>
+//             <div class="ic">
+//               <div class="l">
+//                 <i class="fa-regular fa-trash-can"></i><span>Trash</span>
+//               </div>
+//               <!-- <div class="rating">12</div> -->
+//             </div>
+//             <div class="ic">
+//               <div class="l l3">
+//                 <i class="fa-solid fa-circle-info"></i><span>Important</span>
+//               </div>
+//               <div class="rating r3">50</div>
+//             </div>
+//             <div class="ic">
+//               <div class="l">
+//                 <i class="fa-regular fa-envelope"></i><span>Spam</span>
+//               </div>
+//               <!-- <div class="rating">12</div> -->
+//             </div>
+//             <div class="ic">
+//               <div class="l">
+//                 <i class="fa-regular fa-star"></i><span>Starred</span>
+//               </div>
+//               <!-- <div class="rating">12</div> -->
+//             </div>
+//           </div>
+//           <!-- ======================================== -->
+//           <div class="message">
+//             <div class="msg"><p>Label</p></div>
+//           </div>
+//           <div class="sideBar side2">
+//             <div class="ic">
+//               <div class="l">
+//                 <div class="div div1"></div>
+//                 <span>Private</span>
+//               </div>
+//               <!-- <div class="rating">12</div> -->
+//             </div>
+//             <div class="ic">
+//               <div class="l ll">
+//                 <div class="div div2"></div>
+//                 <span>Family</span>
+//               </div>
+//               <div class="rating rs1">06</div>
+//             </div>
+//             <div class="ic">
+//               <div class="l ll">
+//                 <div class="div div3"></div>
+//                 <span>Teachers</span>
+//               </div>
+//               <!-- <div class="rating">12</div> -->
+//             </div>
+//             <div class="ic">
+//               <div class="l ll">
+//                 <div class="div div4"></div>
+//                 <span>Friend</span>
+//               </div>
+//               <div class="rating rs2">80</div>
+//             </div>
+//             <div class="ic">
+//               <div class="l ll">
+//                 <div class="div div5"></div>
+//                 <span>Work</span>
+//               </div>
+//               <!-- <div class="rating">12</div> -->
+//             </div>
+//           </div>
+//         </div>
+//      `;     
+// })
